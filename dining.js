d176e5c6f7f12dcf569becc993203372fe2c00bb
Coderhehe <77240747+Coderhehe@users.noreply.github.com>
2021-09-25 11:22:09 -0400

    Add files via upload

---
+++ dining.js
@@ -0,0 +1,24 @@
+img="";
+function preload() {
+    img=loadImage("dining.jpg");
+}
+function setup() {
+    canvas=createCanvas(800,500);
+    canvas.center();
+    objectdectect=ml5.objectDetector("cocossd",modelLoaded);
+    document.getElementById("status").innerHTML="Status:Object Detecting";
+}
+function gotResult(error,result) {
+    if (error) {
+        console.error();
+    }
+    else{
+        console.log(result);
+        object=result[0].label;  
+        document.getElementById("objects").innerHTML=object;
+    }
+}
+
+function draw() {
+    image(img,0,0,800,500);
+}
